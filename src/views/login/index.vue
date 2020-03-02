7f012e7d0beb6fd0bef543ce4a8d6a9442bcabd2
天明 <1154706249@qq.com>
2020-03-02 20:03:40 +0800

    登录效验与axios请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,31 +4,32 @@
             <div class="title" style="user-select:none">
                 <img src="../../assets/cat01.png" alt="" style="height:60px">
             </div>
-            <el-form style="margin-top:20px" >
+            <!-- 绑定属性 -->
+            <el-form  :model="loginForm" :rules="loginRules" ref="myForm" style="margin-top:20px" >
               <!-- 手机号 -->
-              <el-form-item >
-                <el-input  placeholder="请输入手机号">
+              <el-form-item prop="mobile">
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号">
                   <template slot="prepend"><i class="el-icon-mobile-phone"></i></template>
                 </el-input>
               </el-form-item>
               <!-- 密码 -->
-              <el-form-item >
-                <el-input  placeholder="请输入密码">
+              <el-form-item prop="password">
+                <el-input v-model="loginForm.password" type="password" placeholder="请输入密码">
                   <template slot="prepend"><i class="el-icon-key"></i></template>
                 </el-input>
               </el-form-item>
               <!-- 验证码 -->
-              <el-form-item>
-              <el-input  placeholder="请输入验证码" style="width:65%;"></el-input>
+              <el-form-item prop="code">
+              <el-input v-model="loginForm.code"  placeholder="请输入验证码" style="width:65%;"></el-input>
               <el-button style="float:right" plain>发送验证码</el-button>
               </el-form-item>
               <!-- 隐私条款 -->
-              <el-form-item>
-                <el-checkbox >我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-form-item prop="checked">
+                <el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <!-- 提交 -->
               <el-form-item>
-                <el-button type="warning" style="width:100%">登录</el-button>
+                <el-button @click="login" type="warning" style="width:100%">登录</el-button>
               </el-form-item>
             </el-form>
           </el-card>
@@ -39,7 +40,55 @@
 export default {
   data () {
     return {
-
+      loginForm: {
+        mobile: '',
+        password: '',
+        code: '',
+        checked: false
+      },
+      loginRules: {
+        // 格式校验 required: true为必填项
+        mobile: [{ required: true, message: '您的手机号不能为空' }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '您的手机号格式不正确'
+        }],
+        password: [{ required: true, message: '您的密码不能为空' }, {
+          pattern: /^\d{6}$/,
+          message: '您的密码格式不正确'
+        }],
+        code: [{ required: true, message: '您的手机号不能为空' }, {
+          pattern: /^\d{6}$/,
+          message: '您的密码格式不正确'
+        }],
+        checked: [{
+          // validator自定义效验，required默认效验(无法验证布尔值)
+          validator: function (rule, value, callback) {
+            // true  or  false
+            value ? callback() : callback(new Error('您必须同意我们的条款才能登陆'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 获取表单组件对象validate整体校验
+      this.$refs.myForm.validate().then(() => {
+        // 异步操作
+        this.$axios({
+          url: '/authorizations',
+          data: this.loginForm,
+          method: 'post'
+        }).then(res => {
+          // 本地添加token
+          window.localStorage.setItem('user-token', res.data.data.token)
+          // 添加完毕跳转页面
+          this.$router.push('/home')
+        }).catch(() => {
+          // this.$message({ message: '用户名或密码错误', type: 'error' })
+          this.$message.error('用户名或密码错误')
+        })
+      })
     }
   }
 }
@@ -63,10 +112,10 @@ export default {
   }
   .login-card{
     z-index: 2;
-    width: 650px;
+    width: 490px;
     height: 480px;
     margin: 0 auto;
-    padding: 0 100px 0 60px;
+    padding-right:50px;
     border: none;
     box-shadow: none;
     box-sizing: border-box;
@@ -75,7 +124,7 @@ export default {
     .title{
       height: 100px;
       text-align: center;
-      background:url('../../assets/font.png') 135px 20px no-repeat;
+      background:url('../../assets/font.png') center 20px no-repeat;
       background-size: contain;
     }
   }
