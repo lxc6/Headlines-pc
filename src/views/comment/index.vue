9a39c7ccdfaa196b3d78ae17ac95c802f355297d
天明 <1154706249@qq.com>
2020-03-05 23:57:37 +0800

    打开与关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -19,7 +19,8 @@
             <template slot-scope="obj">
               <el-button size="small"  type="text">修改</el-button>
               <!-- 根据状态显示与隐藏 obj.row该行的数据-->
-              <el-button size="small" type="text">{{obj.row.comment_status?'关闭':'打开'}}评论</el-button>
+              <el-button @click="openOrClose(obj.row)"
+               size="small" type="text">{{obj.row.comment_status?'关闭':'打开'}}评论</el-button>
             </template>
           </el-table-column>
 
@@ -50,6 +51,27 @@ export default {
     formatterBool (row, column, cellValue, index) {
       // row当前行数据，column当前列数据,cellValue当前单元格数据,index当前索引
       return cellValue ? '正常' : '关闭'
+    },
+    openOrClose (row) {
+      const msg = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`是否确定${msg}评论`, '提示').then(() => {
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          // query传参
+          params: { article_id: row.id }, // 文章id
+          // body传参
+          data: {
+            allow_comment: !row.comment_status// 状态要与当前状态相反
+          }
+        }).then(() => {
+          // 弹消息 重新拉取数据
+          this.$message.success(`${msg}评论成功`)
+          this.getComment()
+        }).catch(() => {
+          this.$message.error(`${msg}评论失败`)
+        })
+      })
     }
   },
   created () {
