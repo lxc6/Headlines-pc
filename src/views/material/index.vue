0c2cee456ee98e7f8b4c907a110063f266438a17
天明 <1154706249@qq.com>
2020-03-07 17:30:38 +0800

    素材收藏与删除

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,7 +6,7 @@
             <!-- 添加素材 -->
             <el-row type="flex" justify="end">
                 <!-- :show-file-list是否显示已上传文件列表 , :http-request覆盖默认上传实现自定义-->
-                <el-upload :show-file-list="false" :http-request="uploadfile" action="">
+                <el-upload :show-file-list="false" :http-request="uploadMaterial" action="">
                     <el-button size="small" type="primary">添加素材</el-button>
                 </el-upload>
             </el-row>
@@ -22,8 +22,10 @@
                             <el-row class="operate"
                             style="height:35px;border-radius:0 0 15px 15px;" type="flex"
                             justify="space-around" align="middle">
-                                <i @click="collectFile(item)" class="el-icon-star-on"></i>
-                                <i @click="delFile(item)" class="el-icon-delete-solid"></i>
+                                <i @click="collectMaterial(item)"
+                                  :style="{color:item.is_collected?'red':'black'}"
+                                class="el-icon-star-on"></i>
+                                <i @click="delMaterial(item.id)" class="el-icon-delete-solid"></i>
                             </el-row>
                         </el-card>
                     </div>
@@ -54,6 +56,7 @@
                     >
                 </el-pagination>
             </el-row>
+            <!-- 弹层走马灯 -->
         </el-card>
 </template>
 <script>
@@ -71,6 +74,7 @@ export default {
     }
   },
   methods: {
+    // 获取并显示
     getMaterial () {
       this.loading = true
       this.$axios({
@@ -87,16 +91,18 @@ export default {
         this.loading = false
       })
     },
+    // 切换标签
     changeTab () {
       this.getMaterial()
     },
+    // 页数
     changePage (newPage) {
       this.page.currentPage = newPage
       this.getMaterial()
     },
-    uploadfile (params) {
+    // 上传
+    uploadMaterial (params) {
       // params为自定义上传参数对象{} params.file为请求参数
-    //   debugger
       const data = new FormData()
       data.append('image', params.file)
       this.$axios({
@@ -110,13 +116,36 @@ export default {
         this.$message.error('上传素材失败')
       })
     },
-    // 收藏
-    collectFile (row) {
-      this.$axios({}).then(() => {}).catch(() => {})
+    // 收藏 item为每一项
+    collectMaterial (item) {
+      this.$axios({
+        method: 'put',
+        url: `/user/images/${item.id}`,
+        data: { collect: !item.is_collected }// 实现点击切换收藏状态
+      }).then(res => {
+        this.getMaterial()
+        if (res.data.collect) {
+          this.$message.success('添加收藏成功')
+        } else {
+          this.$message.success('取消收藏成功')
+        }
+      }).catch(() => {
+        this.$message.success('收藏失败')
+      })
     },
     // 删除
-    delFile (row) {
-      this.$axios({}).then(() => {}).catch(() => {})
+    delMaterial (id) {
+      this.$confirm('你确定要删除该素材吗？').then(() => {
+        this.$axios({
+          method: 'delete',
+          url: `/user/images/${id}`
+        }).then(() => {
+          this.getMaterial()
+          this.$message.success('删除成功')
+        }).catch(() => {
+          this.$message.success('删除失败')
+        })
+      })
     }
 
   },
